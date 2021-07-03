99fe1c6573de514ea259b7e437a1e1d31d5b58a3
ToshiroZ <48633791+ToshiroZ@users.noreply.github.com>
2021-07-03 09:17:52 -0400

    Fix auto update

--- Courvix-VPN/Forms/MainForm.cs
+++ Courvix-VPN/Forms/MainForm.cs
@@ -128,6 +128,10 @@ namespace Courvix_VPN
                     statuslbl.Text = "Status: Downloading Update";
                     var bytes = await Client.GetByteArrayAsync(clientversion.DownloadLink);
                     var fileName = Environment.GetCommandLineArgs().First();
+                    if (File.Exists(Path.Combine(Path.GetTempPath(), Path.GetFileName(fileName))))
+                    {
+                        File.Delete(Path.Combine(Path.GetTempPath(), Path.GetFileName(fileName)));
+                    }
                     File.Move(fileName, Path.Combine(Path.GetTempPath(), Path.GetFileName(fileName)));
                     File.WriteAllBytes(fileName, bytes);
                     Process.Start(fileName);
